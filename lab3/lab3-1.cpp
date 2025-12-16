4f32255cc3aec5aa3b9fdf239724fb43f6243115
ThiriZueZun <684244101@mail.pbru.ac.th>
2025-12-16 14:13:26 +0700

    Add files via upload

---
+++ lab3/lab3-1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+int main(){
+    int pos;
+    std::ifstream file("subject.txt");
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open Subject" << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+        pos = line.find("love");
+        if (pos >= line.length()) {
+            std::cout << "'love' not found in this line." << std::endl;
+        } else {
+            std::cout << "'love' found at position: " << pos << std::endl;
+        }
+    }
+
+    file.close();
+    return 0;
+}
