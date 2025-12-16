015bb7c45a09490582dddea358a7e7800841cd57
ThiriZueZun <684244101@mail.pbru.ac.th>
2025-12-16 14:45:26 +0700

    Update lab3-1.cpp

--- lab3/lab3-1.cpp
+++ lab3/lab3-1.cpp
@@ -3,7 +3,7 @@
 #include<string>
 
 int main(){
-    int pos;
+    int pos, pos2;
     std::ifstream file("subject.txt");
 
     if (!file.is_open()) {
@@ -11,18 +11,47 @@ int main(){
         return 1;
     }
 
-    std::string line;
+    // std::string line;
+    // while (std::getline(file, line)) {
+    //     std::cout << line << std::endl;
+
+    //     pos = line.find("love");
+    //     if (pos >= line.length()) {
+    //         std::cout << "'love' not found in this line." << std::endl;
+    //     } else {
+    //         std::cout << "'love' found at position: " << pos << std::endl;
+    //     }
+    // }
+
+     std::string line;
     while (std::getline(file, line)) {
         std::cout << line << std::endl;
-        pos = line.find("love");
+        // transform line to lowercase for case-insensitive search
+        std::string lowerLine = line;
+        for (char& c : lowerLine) {
+            c = std::tolower(c);
+        }
+
+        pos = lowerLine.find("love");
+        pos2 = lowerLine.find("love", pos + 1); // this will always return npos since "Love" != "love"
         if (pos >= line.length()) {
-            std::cout << "'love' not found in this line." << std::endl;
+            std::cout << "'love' not found in the line." << std::endl;
         } else {
             std::cout << "'love' found at position: " << pos << std::endl;
+        }
+
+        //tokenize the line into words
+        size_t start = 0;
+        size_t end = lowerLine.find(' ');
+        bool foundword = false;
+        while (end != std::string::npos) {
+            std::string word = lowerLine.substr(start, end - start);
+            start = end + 1;
+            end = lowerLine.find(' ', start);
+            std::cout << "Token: " << word << std::endl;
         }
     }
 
     file.close();
     return 0;
-
 }
